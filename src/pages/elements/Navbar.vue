bf0a3a112e2af165ecae6dd12dd3d82b3b2a7990
Spiralis-Solutions <krisstjohn96@gmail.com>
2018-05-30 11:48:17 -0700

    f

--- src/pages/elements/Navbar.vue
+++ src/pages/elements/Navbar.vue
@@ -1,14 +1,6 @@
 <template>
   <div class="main">
-    <div class="logo" v-on:click="$router.push('/')">Logo</div>
-    <div class="navbutton" v-on:click="navpaneLogic = !navpaneLogic"></div>
-    <div class="navpane" v-if="navpaneLogic">
-      <div class="search" v-on:click="navpaneLogic = false; $router.push('/search')" v-if="logged">Search</div>
-      <div class="account" v-on:click="navpaneLogic = false; $router.push('/account')" v-if="logged">Account</div>
-      <div class="submit" v-on:click="navpaneLogic = false; $router.push('/submit')" v-if="logged">Submit</div>
-      <div class="login" v-on:click="navpaneLogic = false; $router.push('/login')" v-if="!logged">Login</div>
-      <div class="register" v-on:click="navpaneLogic = false; $router.push('/register')" v-if="!logged">Register</div>
-    </div>
+    <div class="logo" v-on:click="$router.push('/')"></div>
   </div>
 </template>
 
@@ -26,98 +18,27 @@ export default {
 
 <style scoped lang="less">
   .main {
-    position: absolute;
+    position: fixed;
     top: 0;
     left: 0;
     right: 0;
-    height: 120px;
+    height: 55px;
     background-color: #222;
-    opacity: .8;
+    background-image:url('../../assets/noise2.jpg');
     display: grid;
-    grid-template-columns: repeat(5, 1fr);
-    grid-template-rows: 120px 400px;
-    margin-bottom: 100px;
+    grid-template-columns: repeat(3, 1fr);
     z-index: 5;
+    box-shadow: 0px 1px 5px black;
   }
 
   .logo {
-    grid-column-start: 1;
-    grid-column-end: 3;
-    width: 140px;
-    height: 100px;
-    background-color: #666;
-    opacity: .6;
-    line-height: 100px;
-    font-size: 3em;
-    padding-left: 20px;
-    margin-left: 20px;
-    margin-top: 10px;
-  }
-
-  .navbutton {
-    width: 100px;
-    background-image: url('../../assets/hamburger.svg');
+    background-image: url('../../assets/cougarVille.png');
+    background-position: center;
     background-repeat: no-repeat;
-    grid-column: 4;
-    margin-left: 20px;
-  }
-
-  .navpane {
+    background-size: contain;
+    grid-column: 2;
     width: 100%;
-    height: 400px;
-    grid-column-start: 2;
-    grid-column-end: 5;
-    grid-row: 2;
-    display: grid;
-    grid-template-rows: repeat(6, 1fr);
-    background-color: #666;
-    opacity: .6;
-    border-radius: 10px;
-    margin-top: 10px;
-  }
-
-  .search {
-    grid-row: 1;
-    width: 100%;
-    font-size: 2em;
-    color: #fff;
-    text-align: center;
-  }
-
-  .forum {
-    grid-row: 2;
-    width: 100%;
-    font-size: 2em;
-    color: #fff;
-    text-align: center;
-  }
-
-  .login {
-    grid-row: 3;
-    width: 100%;
-    font-size: 2em;
-    color: #fff;
-    text-align: center;
-  }
-  .register {
-    grid-row: 4;
-    width: 100%;
-    font-size: 2em;
-    color: #fff;
-    text-align: center;
-  }
-  .account{
-    grid-row: 3;
-    width: 100%;
-    font-size: 2em;
-    color: #fff;
-    text-align: center;
-  }
-  .submit {
-    grid-row: 4;
-    width: 100%;
-    font-size: 2em;
-    color: #fff;
-    text-align: center;
+    height: 100%;
+    z-index: 10;
   }
 </style>
